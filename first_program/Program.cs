4067f843818ba41a3ecd1e2ab55f25a519083a9f
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-08-08 18:48:44 +0500

    first program in C-sharp

---
+++ first_program/Program.cs
@@ -0,0 +1,12 @@
+﻿// Console.WriteLine()=>Create new line
+// Console.Write()=>Don't Create new line
+//Console.Beep()=>After program run they create beep sound
+//Console.ReadKey()=>Console Window ma enter ka baad path show krwae ga
+
+
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Today I started to learn C-sharp language from brocode youtube channel");
+Console.Beep();
+Console.ReadKey();
+
