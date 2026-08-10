070f20a9e32570934fb074b6f442d880704e3824
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-08-10 22:00:50 +0500

    userInput

---
+++ 02_userInput/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02_userInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learn about user input");
+
+            // Console.ReadLine() => Input leta hn or string return krta hn
+
+            Console.WriteLine("What's your name?");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("What's your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Hello {name} and you are {age} year old ");
+        }
+    }
+}
