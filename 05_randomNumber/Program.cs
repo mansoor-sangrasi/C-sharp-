3d40014927d871401dd7ce0c690cbf57f6db60b3
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-08-13 22:04:28 +0500

    randomNumber

---
+++ 05_randomNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _05_randomNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learn how to generate random number in C-sharp");
+
+            // first of all create random object
+            Random random = new Random();
+
+            // Generate random number b/w whole number
+            int randomWholeNum = random.Next(1, 7); // random num b/w 1 & 6
+            Console.WriteLine(randomWholeNum);
+
+            // Generate random number b/w decimal number
+            double randomDecimalNum = random.NextDouble(); // No argument accept
+            Console.WriteLine($"decimal number b/w 0 and 1 is {randomDecimalNum}");
+                
+
+        }
+    }
+}
