0c5ffbbb75aa102006b5e8955471413ef41d56be
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-08-16 15:51:54 +0500

    switch

---
+++ 08_switch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace _08_switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learn about Switch in C-sharp");
+
+            Console.WriteLine("What is Today day");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            switch (day){
+                case 1:
+                    Console.WriteLine("Today is Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Today is Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Today is Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Today is Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Today is Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Today is Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Today is Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Please provide valid day");
+                   break;
+
+
+            }
+        }
+    }
+}
