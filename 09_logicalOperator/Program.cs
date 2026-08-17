0a1815d1277f1bf045374b61b19f05ceaa14bcf6
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-08-17 13:03:59 +0500

    logicalOperator

---
+++ 09_logicalOperator/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _09_logicalOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // logical operator is used to check more than one condition
+            // AND &&
+            // OR ||
+
+            Console.WriteLine("Learn about logical operator");
+
+            Console.WriteLine("Enter Temperatur:");
+            double temp = Convert.ToDouble(Console.ReadLine());
+
+            if (temp>=0 && temp<=35)
+            {
+                Console.WriteLine("Today weather was cool");
+            }
+            else if (temp>=36)
+            {
+                Console.WriteLine("Today weather was hot");
+            }
+        }
+    }
+}
