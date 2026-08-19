29f3ae966b85ca9523b21882e41ffce8b7e5bfd0
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-08-19 19:02:11 +0500

    forLoop

---
+++ 11_forLoop/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _11_forLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            // for loop => Repeat some code in finite amount of times
+
+            Console.WriteLine("Learn for loop in C-sharp");
+
+
+            for (int i=1; i<=10; i++)
+            {
+                
+                Console.WriteLine($"7 x {i} = {7 * i}");
+            }
+        }
+    }
+}
