e859fb2397ebb8b43df2c063443e9bb78550c12d
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-08-20 19:22:38 +0500

    nestedLoop

---
+++ 12_nestedLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _12_nestedLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // nested loop => loop inside of other loop
+            // it is used in sorting algorithm
+            Console.WriteLine("Learn nested loop in C-sharp");
+
+            Console.Write("How many row in rectangle:");
+            int row = Convert.ToInt32(Console.ReadLine());
+            Console.Write("How many column in rectangle:");
+            int column = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=1; i<=row; i++)
+            {
+                for (int j=1; j<=column; j++)
+                {
+                    Console.Write("*");
+                    
+                }
+                Console.WriteLine();
+
+            }
+
+
+        }
+    }
+}
