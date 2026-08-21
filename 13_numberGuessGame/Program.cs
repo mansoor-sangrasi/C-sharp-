c12c813efdbf15db94c334bf65b606ab8a503259
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-08-21 22:19:06 +0500

    numberGuessGame

---
+++ 13_numberGuessGame/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _13_numberGuessGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("Guess number between 1 and 100");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            int computerNum = random.Next(1, 101);
+
+
+            if (userNum == computerNum)
+            {
+                Console.WriteLine("Correct Guess");
+            }
+            else if (userNum < computerNum)
+            {
+                Console.WriteLine("Guess Number is less than Computer Number");
+            }
+            else if (userNum > computerNum)
+            {
+                Console.WriteLine("Guess Number is greater than Computer Numnber");
+            }
+
+            while (userNum != computerNum)
+            {
+                Console.WriteLine("Guess number between 1 and 100");
+                userNum = Convert.ToInt32(Console.ReadLine());
+
+                if (userNum == computerNum)
+                {
+                    Console.WriteLine("Correct Guess");
+                }
+                else if (userNum < computerNum)
+                {
+                    Console.WriteLine("Guess Number is less than Computer Number");
+                }
+                else if (userNum > computerNum)
+                {
+                    Console.WriteLine("Guess Number is greater than Computer Numnber");
+                }
+            }
+        }
+    }
+}
