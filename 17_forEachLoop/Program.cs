90963742180f7853c9c8603b2977794f92cd08f9
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-08-25 22:02:12 +0500

    forEachLoop

---
+++ 17_forEachLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _17_forEachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* foreach loop => A simpler way to iterate over an array but it's
+            less flexible */
+
+            String[] people = { "Mansoor Ahmed", "Majid Junejo", "Tufail Ahmed" };
+            foreach (String person in people)
+            {
+                Console.WriteLine(person);
+
+            }
+        }
+    }
+}
