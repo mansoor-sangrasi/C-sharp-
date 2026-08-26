29b62731d0f7a756a23d21408a7e41a1e01ccaa7
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-08-26 13:23:32 +0500

    function

---
+++ 18_function/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _18_function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* function => perform section of code whenever it's called invoked method
+             * it's benefit to reuse of code without writing the code */
+
+            String name = "Mansoor Ahmed";
+            String name1 = "Abdul Kareem";
+
+            singHappyBirthDay(name);
+            singHappyBirthDay(name1);
+        }
+
+        static void singHappyBirthDay (String name)
+        {
+            Console.WriteLine("Happy BirthDay");
+            Console.WriteLine(name);
+
+        }
+    }
+}
