8d73ede941ffcfd4e0b09e9a2ee9338ab4517744
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-08-27 22:21:38 +0500

    returnFunction

---
+++ 19_returnFunction/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _19_returnFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // return => Return back data where method is invoked
+
+            int x;
+            int y;
+            int result;
+
+            Console.Write("Enter value of x:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter value of y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+             result = Addition(x, y);
+            Console.WriteLine($"Addition result : {result}");
+        }
+        static int Addition (int x, int y)
+        {
+            int z = x + y;
+            return z;
+        }
+    }
+}
