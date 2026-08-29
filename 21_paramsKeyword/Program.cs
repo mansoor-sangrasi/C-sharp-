29368ca80fc62f59155a21360466d940b89f0f89
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-08-29 12:45:20 +0500

    paramsKeyword

---
+++ 21_paramsKeyword/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _21_paramsKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // params keyword => With the help of params keyword a single 
+            // function can takes multiple argument in form of single dimensional
+            // array.
+            // it's avoid function overloading.
+
+            totalPrice(200, 300, 500); // => method invoked
+        }
+        static void totalPrice (params int[] prices)
+        {
+            int total = 0;
+            for (int i=0; i<prices.Length; i++)
+            {
+                total += prices[i];
+
+            }
+            Console.WriteLine($"TotalPrice = {total}");
+
+        }
+    }
+}
