7189701581618c3292292f0bfcf2830fbae8b3c8
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-08-31 20:12:03 +0500

    ternaryOperator

---
+++ 23_ternaryOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _23_ternaryOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ternary operator or conditional operator
+            // (condition) ? true statement : false statement;
+
+            int temperature = 20;
+            String message;
+
+            message = (temperature >= 15) ? "it's warm outside" : "it's cool outside";
+            Console.WriteLine(message);
+        }
+    }
+}
