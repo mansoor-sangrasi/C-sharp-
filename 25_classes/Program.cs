f98033e2c3b03e8d559393f66b676e0ffc117b30
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-09-02 21:16:02 +0500

    classes

---
+++ 25_classes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _25_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // classes => A bundle of code can be used as a blueprint
+            // to create object.
+            // static class => object not necessary to create class to show helper
+            //method.
+            messageClass.Hello();
+            messageClass.Waiting();
+            messageClass.Bye();
+        }
+    }
+}
---
+++ 25_classes/messageClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _25_classes
+{
+    static class messageClass
+    {
+        static public void Hello ()
+        {
+            Console.WriteLine("Hello dear");
+        }
+
+        static public void Waiting () 
+        {
+            Console.WriteLine("I am waiting for you");
+        }
+        static public void Bye ()
+        {
+            Console.WriteLine("Bye see you tomorrow");
+
+        }
+
+    }
+}
