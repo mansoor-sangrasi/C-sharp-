840f10f8b82348269ad4c85c7e3776ace432d647
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-09-03 21:55:09 +0500

    object

---
+++ 26_object/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _26_object
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            // object => Instance of a class.
+            // object have fields and method.
+
+            Human human1 = new Human();
+            Human human2 = new Human();
+
+            human1.name = "Mansoor Ahmed";
+            human1.age = 19;
+
+            human2.name = "Muhammad Siddique";
+            human2.age = 52;
+
+            human1.Eat();
+            human1.Sleep();
+
+            human2.Eat();
+            human2.Sleep();
+
+
+            
+        }
+    }
+
+    class Human
+    {
+        public String name;
+        public int age;
+
+        public  void Eat ()
+        {
+            Console.WriteLine($"{name} is eating");
+
+        }
+        public void Sleep ()
+        {
+            Console.WriteLine($"{name} is sleeping");
+        }
+    }
+
+
+}
