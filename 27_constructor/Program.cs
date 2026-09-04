9587db463d9800da51cbc1c560266a292a7a3650
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-09-04 20:19:31 +0500

    constructor

---
+++ 27_constructor/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _27_constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // constructor => A special method in class having same name
+            // can be used to assign argument when creating the object.
+
+            Car car1 = new Car("Honda", "City", 2026, "Black");
+            Car car2 = new Car("Honda","Swift",2024,"White");
+
+            
+            car1.drive();
+            car2.drive();
+        }
+    }
+
+    class Car
+    {
+        String make;
+        String model;
+        int year;
+        String color;
+
+        public Car (String make, String model, int year, String color)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.color = color;
+
+        }
+
+        public void drive ()
+        {
+            Console.WriteLine($"You drive the {make} {model}");
+        }
+    }
+}
