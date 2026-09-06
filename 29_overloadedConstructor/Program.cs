53b7298047f387270e3c32bab8ec313e3b45b7cf
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-09-06 12:32:56 +0500

    overLoadedConstructor

---
+++ 29_overloadedConstructor/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _29_overloadedConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // overloaded constructor => Technique to create multiple constructor
+            // with different set of parameter.
+            // name + parameter = signature
+
+            Pizza pizza1 = new Pizza("stuffed crust", "red souce", "mozzarella", "pepperoni");
+            Pizza pizza2 = new Pizza("stuffed crust", "red souce", "mozzarella");
+            Pizza pizza3 = new Pizza("stuffed crust", "red souce");
+            Pizza pizza4 = new Pizza("stuffed crust");
+
+        }
+    }
+
+    class Pizza
+    {
+        String bread;
+        String sauce;
+        String cheese;
+        String topping;
+
+        public Pizza(String bread, String sauce, String cheese, String topping)
+        {
+
+        }
+        public Pizza(String bread, String sauce, String cheese)
+        {
+
+        }
+        public Pizza(String bread, String sauce)
+        {
+
+        }
+        public Pizza(String bread)
+        {
+
+        }
+    }
+}
