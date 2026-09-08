3b7834845251cd7de721ff3f8655c25fdcfffd3e
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-09-08 12:01:40 +0500

    abstractClass

---
+++ 31_abstractClass/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _31_abstractClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // abstract class => Modifier that indicate missing component and 
+            // incomplete implementation .
+            // class having no object.
+
+
+            Car car = new Car();
+            Bike bike = new Bike();
+            Boat boat = new Boat();
+            
+          
+            
+        }
+    }
+
+   abstract class Vehicle
+    {
+        public int speed = 0;
+
+        public void go()
+        {
+            Console.WriteLine("This vehicle is moving");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public int wheel = 4;
+       public int maxSpeed = 200;
+    }
+
+    class Bike : Vehicle
+    {
+        public int wheel = 2;
+       public int maxSpeed = 120;
+    }
+
+    class Boat : Vehicle
+    {
+        public int wheel = 0;
+       public int maxSpeed = 150;
+    }
+}
+
+
