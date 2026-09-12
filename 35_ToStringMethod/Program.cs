fef590e2474dbffa5a642dfe579f93bb12638d8a
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-09-12 12:46:46 +0500

    toStringMethod

---
+++ 35_ToStringMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _35_ToStringMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ToString() => convert object to string representation that is suitable
+            // for display.
+
+            Car car = new Car("Honda", "Civic", 2026);
+
+            Console.WriteLine(car);
+        }
+    }
+
+    class Car
+    {
+         String make;
+         String model;
+         int year;
+
+        public Car(String make, String model, int year)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+
+        }
+
+        public override string ToString()
+        {
+            String message = $"This is a {make} {model}";
+            return message;
+        }
+    }
+}
