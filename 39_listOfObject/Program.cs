fdbbaf25a07a2557a4a04f02f2f89190bf6d9c9f
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-09-16 20:52:12 +0500

    listOfObject

---
+++ 39_listOfObject/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _39_listOfObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player("Mansoor Ahmed");
+            Player player2 = new Player("Muhammad Siddique");
+            Player player3 = new Player("Ayesha");
+
+            // list of object
+
+            List<Player> players = new List<Player>();
+
+            players.Add(player1);
+            players.Add(player2);
+            players.Add(player3);
+
+            foreach(Player player in players)
+            {
+                Console.WriteLine(player.userName);
+
+            }
+            
+        }
+    }
+
+    class Player
+    {
+        public String userName;
+
+        public Player(String userName)
+        {
+            this.userName = userName;
+
+        }
+    }
+}
