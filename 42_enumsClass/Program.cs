d264169c48c3134c32df1d9eb0c502e99b1eef82
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-09-19 10:29:33 +0500

    enumsClass

---
+++ 42_enumsClass/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _42_enumsClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // enum class => A special class contain set of constant integer value
+            // and each integer value have meaningful name
+            // to get the integer value from an item you must convert explicitly
+            // to an int
+
+            Console.WriteLine($"{Planetgravity.Earth} gravity is {(int)Planetgravity.Earth}");
+        }
+    }
+
+    enum Planetgravity
+    {
+        Mercury = 4,
+        Venus = 9,
+        Earth =  10,
+        Mars = 4,
+        Jupiter = 25,
+        Saturn = 10,
+        Uranus = 9,
+        Neptune = 11
+
+    
+    
+    }
+
+}
