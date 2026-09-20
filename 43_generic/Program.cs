fb91224ec0c5623fc864290c8b5f177282ed05db
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-09-20 20:39:56 +0500

    generic

---
+++ 43_generic/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _43_generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generic => No particular data type
+            // add <T> or <Thing> to classes,methods,fields
+            // allow code reusability for different data type
+
+            int[] intArray = { 71, 72, 73 };
+            double[] doubleArray = { 1.7, 3.01, 3.7 };
+            String[] stringArray = { "Mansoor Ahmed", "Tofique Ahmed", "Tufail Ahmed" };
+
+            displayElement(intArray);
+            displayElement(doubleArray);
+            displayElement(stringArray);
+        }
+
+         static void displayElement<Thing>(Thing[] array)
+        {
+            foreach(Thing itemArray in array)
+            {
+                Console.Write(itemArray + " ");
+
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
