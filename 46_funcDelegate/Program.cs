bb848e7000a365f619d9dac7df4e436d54e82a7b
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-09-23 22:11:39 +0500

    funcDelegate

---
+++ 46_funcDelegate/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _46_funcDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // func delegate is used for return type method
+            // this delegate take zero or more parameter and last parameter is
+            // consider out parameter means return type of method
+
+            Func<String,int,String> func_message = message;
+            Console.WriteLine(func_message("Mansoor Ahmed",71));
+        }
+
+        public static String message(String msg, int rollNo)
+        {
+            return "Hello " + msg  + rollNo;
+        }
+    }
+
+   
+}
