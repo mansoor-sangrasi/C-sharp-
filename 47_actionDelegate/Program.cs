6c44f91d14b57482d200f20af7a89e0b2e15e354
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-09-24 23:36:35 +0500

    actionDelegate

---
+++ 47_actionDelegate/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _47_actionDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // action delegate is used for void type method
+            Action<int, int> actionDelegate = multiply;
+            actionDelegate(10, 2);
+        }
+
+        public static void multiply(int a, int b)
+        {
+            Console.WriteLine($"Multiplication : {a * b}");
+
+        }
+    }
+}
